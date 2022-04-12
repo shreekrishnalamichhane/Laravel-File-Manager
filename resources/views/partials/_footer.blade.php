6bc4d56c8135001c87973faca2b7c6af8bd24d7f
Shree Krishna Lamichhane <47468714+shreekrishnalamichhane@users.noreply.github.com>
2022-04-12 21:57:48 +0545

    footer text and links updated

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -4,22 +4,22 @@
         <div class="container">
             <div class="d-md-flex justify-content-between pt-4">
                 <div class="pb-4 fs-xs text-muted text-center text-md-start">© All rights reserved. By <a
-                        class="text-muted" href="#">Bsc Csit Nepal</a></div>
+                        class="text-muted" href="#">Shree Krishna Lamichhane</a></div>
                 <div class="widget widget-links pb-4">
                     <ul class="widget-list d-flex flex-wrap justify-content-center justify-content-md-start">
-                        <li class="widget-list-item ms-4"><a class="widget-list-link fs-ms"
-                                href="https://bsccsitnepal.com" target="_blank">Our Website</a>
+                        <li class="widget-list-item ms-4"><a class="widget-list-link fs-ms" href="#" target="_blank">Our
+                                Website</a>
                         </li>
-                        <li class="widget-list-item ms-4"><a class="widget-list-link fs-ms"
-                                href="https://facebook.com/bsccsitnepal2075" target="_blank">Facebook</a>
+                        <li class="widget-list-item ms-4"><a class="widget-list-link fs-ms" href="#"
+                                target="_blank">Facebook</a>
                         </li>
-                        <li class="widget-list-item ms-4"><a class="widget-list-link fs-ms"
-                                href="https://bsccsitnepal.com/home/about_us" target="_blank">About</a></li>
-                        <li class="widget-list-item ms-4"><a class="widget-list-link fs-ms"
-                                href="https://bsccsitnepal.com/home/privacy_policy" target="_blank">Privacy Policy</a>
+                        <li class="widget-list-item ms-4"><a class="widget-list-link fs-ms" href="#"
+                                target="_blank">About</a></li>
+                        <li class="widget-list-item ms-4"><a class="widget-list-link fs-ms" href="#"
+                                target="_blank">Privacy Policy</a>
                         </li>
-                        <li class="widget-list-item ms-4"><a class="widget-list-link fs-ms"
-                                href="https://bsccsitnepal.com/home/terms_and_condition" target="_blank">Terms &
+                        <li class="widget-list-item ms-4"><a class="widget-list-link fs-ms" href="#"
+                                target="_blank">Terms &
                                 Conditions</a>
                         </li>
                     </ul>
